33e1a582e2c68027c108307a0a847bcfd2b00434
Saranya K <saranya.k@soprasteria.com>
2021-10-20 11:28:50 +0530

    Initial Commit

---
+++ src/main/java/com/learning/trackerapi/TrackerApiApplication.java
@@ -0,0 +1,14 @@
+package com.learning.trackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerApiApplication.class, args);
+		System.out.println("data");
+	}
+
+}
